ecdeadc194bd61f3bd6dabe8afc24fb272538b22
Corsaka <skullzlegrys@hotmail.com>
2024-03-14 20:51:48 +0000

    remove run files

--- run/launch.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Java\jdk-17.0.10\bin\java.exe" -Dfabric.skipMcProvider=true -Dfabric.development=true -classpath "/cosmic-reach.jar;cosmicreach-fabric-modloader-1.1.0.jar;deps/*" net.fabricmc.loader.launch.knot.KnotClient
-
